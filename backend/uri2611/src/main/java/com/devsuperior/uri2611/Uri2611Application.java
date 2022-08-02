3167c41196f0e7f977127c2e033a3221d559d437
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-02 16:05:03 -0300

    Projetos Base para iniciar os Estudos de Casos de Uso com SQL e JPQL

---
+++ backend/uri2611/src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -0,0 +1,12 @@
+package com.devsuperior.uri2611;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2611Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2611Application.class, args);
+	}
+}
