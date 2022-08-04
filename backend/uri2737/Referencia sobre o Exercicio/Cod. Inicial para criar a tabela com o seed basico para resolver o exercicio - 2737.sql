64e4bd98259aec5caa82074e9d71b3615cae1343
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-04 14:48:05 -0300

    Resolvendo uri2737: Utilizando apenas o SQL padrao, pois o JPQL ainda nao possui UNIAO

--- backend/uri2737/Referencia sobre o Exercicio/Cod. Inicial para criar a tabela com o seed basico para resolver o exercicio - 2737.sql
+++ backend/uri2737/Referencia sobre o Exercicio/Cod. Inicial para criar a tabela com o seed basico para resolver o exercicio - 2737.sql
@@ -20,3 +20,41 @@ CREATE TABLE lawyers(
   
   /*  Execute this query to drop the tables */
   -- DROP TABLE lawyers; --
+
+* SOLUÇÃO 01:
+ 
+ (SELECT name, customers_number FROM lawyers
+ORDER BY customers_number DESC
+LIMIT 1)
+
+UNION ALL
+
+(SELECT name, customers_number FROM lawyers
+ORDER BY customers_number ASC
+LIMIT 1)
+
+UNION ALL
+
+(SELECT 'Average', ROUND(AVG(customers_number), 0)
+FROM lawyers)
+
+* SOLUÇÃO ALTERNATIVA PARA EVITAR O ORDER BY (Comando um pouco PESADO)
+UTILIZANDO OS COMANDOS DO SQL MAX E MIN COMO UMA SUBCONSULTA:
+
+(SELECT name, customers_number FROM lawyers
+WHERE customers_number = (
+    SELECT MAX(customers_number) FROM lawyers
+))
+
+UNION ALL
+
+(SELECT name, customers_number FROM lawyers
+WHERE customers_number = (
+    SELECT MIN(customers_number) FROM lawyers
+))
+
+UNION ALL
+
+(SELECT 'Average', ROUND(AVG(customers_number), 0)
+FROM lawyers)
+ 
